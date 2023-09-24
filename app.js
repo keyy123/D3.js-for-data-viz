b87ce9dc9b8dac9d53dfe1fbcced2ee8f3415f8c
keyy123 <parktwins1@gmail.com>
2023-09-23 23:56:35 -0400

    Completed notes for section and file is up to date

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// Hello world!
+// Exercise 1: Select
+// const nativeBody = document.querySelector('body');
+// const d3Body = d3.select('body');
+
+// console.log('Vanilla:', nativeBody);
+// console.log('D3:', d3Body);
+
+// Exercise 2: SelectAll
+// const d3Picks = d3.selectAll("p");
+// console.log("D3:", d3Picks);
