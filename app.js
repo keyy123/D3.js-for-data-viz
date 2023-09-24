d9e110bb108610340360e13eaf3fdc9d82afa019
keyy123 <parktwins1@gmail.com>
2023-09-24 00:26:08 -0400

    created notes and updated file to current lesson

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-// Exercise 1: Select
-// const nativeBody = document.querySelector('body');
-// const d3Body = d3.select('body');
-
-// console.log('Vanilla:', nativeBody);
-// console.log('D3:', d3Body);
-
-// Exercise 2: SelectAll
-// const d3Picks = d3.selectAll("p");
-// console.log("D3:", d3Picks);
+// Exercise 1: Append
+const d3Body = d3.select('body').append("p");
+console.log(d3Body);
