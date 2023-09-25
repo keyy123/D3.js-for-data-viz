1ea39e49309d7cc8875af686b0746cd18cb883df
keyy123 <parktwins1@gmail.com>
2023-09-25 19:55:12 -0400

    notes and files done!

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const data = [10, 20, 30, 40, 50];
-const el = d3.selectAll('li')
-    .data(data);
+
+// exercise 1 - using join API
+// const el = d3.selectAll('li')
+//     .data(data)
+//     .join('li');
+
+//exercise 2 - using select + selectAll
+
+const el = d3.select('ul')
+    .selectAll('li')
+    .data(data)
+    .join('li')
+    .text("Hello")
+    // exercise 3 - add text
+
 console.log(el);
