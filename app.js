8717d82387bf6dadaed30f9af29cdba7cb37e701
keyy123 <parktwins1@gmail.com>
2023-09-25 20:11:30 -0400

    notes added & file updated!

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const data = [10, 20, 30, 40, 50];
 
-// exercise 1 - using join API
-// const el = d3.selectAll('li')
-//     .data(data)
-//     .join('li');
-
-//exercise 2 - using select + selectAll
-
 const el = d3.select('ul')
     .selectAll('li')
     .data(data)
     .join('li')
     .text("Hello")
-    // exercise 3 - add text
 
 console.log(el);
