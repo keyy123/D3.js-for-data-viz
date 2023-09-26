bac8d801323e065dd7c7192d18991107410b9e2b
keyy123 <parktwins1@gmail.com>
2023-09-25 20:23:58 -0400

    notes completed and files updated!

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const data = [10, 20, 30, 40, 50];
 const el = d3.select('ul')
     .selectAll('li')
     .data(data)
-    .join('li')
-    .text("Hello")
+    .join("li")
+    .text(d => d)
 
 console.log(el);
