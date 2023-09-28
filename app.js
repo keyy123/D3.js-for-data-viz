ffbcdff060e85c0d7565217106d6d27f5aab472f
keyy123 <parktwins1@gmail.com>
2023-09-28 08:56:55 -0400

    notes and files updated

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
-const data = [10, 20, 30, 40, 50];
+const data = [10,20,30,40,50]
 
-// exercise 1
-/*
-const el = d3.select('ul')
-    .selectAll('li')
-    .data(data) 
-    .join(
-        enter => {
-            // answer: enter.append('li');
-            return enter.append('li');
-        }
-    )
-    .text(d => d)
-*/
-
-// exercise 2 
 const el = d3.select('ul')
     .selectAll('li')
     .data(data)
-    .join(
-        enter => {
-            return enter.append('li')
-                .style('color', 'pink');
-        },
-        update => {
-            console.log(update);
-            return update.style('color', 'green');
-        }
-    )
     .text(d => d)
 
-console.log(el);
+// exercise 1
+el.enter()
+    .append('li')
+    .text(d => d)
+
+// exercise 2 
+el.exit()
+    .remove()
+
+console.log(el)
