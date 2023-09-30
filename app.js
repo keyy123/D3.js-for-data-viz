d6734d647b4fa1279bfdb23f691d4479c1d2a920
keyy123 <parktwins1@gmail.com>
2023-09-30 14:29:58 -0400

    notes updated and file done!

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ async function draw() {
     let dimensions = {
         width: 800,
         height: 800,
+        margin: {
+            top: 50,
+            right: 50,
+            bottom: 50,
+            left: 50
+        }
     }
     const svg = d3.select("#chart")
         .append("svg")
         .attr("width", dimensions.width)
         .attr("height", dimensions.height);
+    
+    const ctr = svg
+        .append("g")
+        .attr("transform",
+        `translate(${dimensions.margin.left}, ${dimensions.margin.top})`    
+    )
+    
+    ctr.append("circle")
+        .attr("r", 15);
     
 }
 
