c84e258072c5118e89e4da64ad9503162f771591
keyy123 <parktwins1@gmail.com>
2023-10-01 19:30:42 -0400

    note and file done!

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 async function draw() {
     const dataset = await d3.json('./data.json');
 
+    // exercise 2 - accessors
+    xAccessor = (d) => d.currently.humidity
+    yAccessor = (d) => d.currently.apparentTemperature
+
     let dimensions = {
         width: 800,
         height: 800,
@@ -21,10 +25,15 @@ async function draw() {
         .attr("transform",
         `translate(${dimensions.margin.left}, ${dimensions.margin.top})`    
     )
-    
-    ctr.append("circle")
-        .attr("r", 15);
-    
+    // exercise 2 - step 3 extra credit
+    ctr.selectAll('circle')
+        .data(dataset)
+        .join('circle')
+        .attr("cx", xAccessor)
+        .attr("cy", yAccessor)
+        .attr("r", 5)
+        .attr("fill", "red")
+
 }
 
 // currently -> humidity + apparentTemperature
