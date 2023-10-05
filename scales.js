7f772167d6a538f5e6fc6b018cc88aac2989d2fd
keyy123 <parktwins1@gmail.com>
2023-10-05 07:24:55 -0400

    notes and files updated

--- scales.js
+++ scales.js
@@ -1,7 +1,7 @@
 const slices = [100, 200, 300, 400, 500]
 
 const scale = d3.scaleLinear()
-    .domain([100, 500])
+    .domain(d3.extent(slices))
     .range([10, 200]);
-
+  
 console.log(scale(475));
